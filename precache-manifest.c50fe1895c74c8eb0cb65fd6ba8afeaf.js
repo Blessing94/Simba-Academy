a8baac81dbaa8ac6e7fed82537059fa2b09e486b
Blessing94 <63746633+Blessing94@users.noreply.github.com>
2021-02-06 16:35:40 +0800

    Updates

---
+++ precache-manifest.c50fe1895c74c8eb0cb65fd6ba8afeaf.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33752eee29c9d95a7a7fb84a68a0758a",
+    "url": "/Simba-Academy/index.html"
+  },
+  {
+    "revision": "8ad9dcc0b6157dc30e63",
+    "url": "/Simba-Academy/static/css/2.092bdb55.chunk.css"
+  },
+  {
+    "revision": "86cb19edeba515380de6",
+    "url": "/Simba-Academy/static/css/main.5573b742.chunk.css"
+  },
+  {
+    "revision": "8ad9dcc0b6157dc30e63",
+    "url": "/Simba-Academy/static/js/2.4aff5ac3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Simba-Academy/static/js/2.4aff5ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86cb19edeba515380de6",
+    "url": "/Simba-Academy/static/js/main.bcfae14f.chunk.js"
+  },
+  {
+    "revision": "ff5b681a2c64a28fea5d",
+    "url": "/Simba-Academy/static/js/runtime-main.20f56595.js"
+  },
+  {
+    "revision": "c70f77c004b5c956eae0dd9bef9835f0",
+    "url": "/Simba-Academy/static/media/img1.c70f77c0.jpg"
+  },
+  {
+    "revision": "49aac920794740d73bd9ce911e2c3f68",
+    "url": "/Simba-Academy/static/media/img10.49aac920.jpeg"
+  },
+  {
+    "revision": "8dc3f7b503348728b7a0570c9d0cbbd0",
+    "url": "/Simba-Academy/static/media/img2.8dc3f7b5.jpg"
+  },
+  {
+    "revision": "e71cd523dc3592b470f9b2b24294a80d",
+    "url": "/Simba-Academy/static/media/img3.e71cd523.jpg"
+  },
+  {
+    "revision": "d01a90460633f7bc2a8da8bd3b1c6e2e",
+    "url": "/Simba-Academy/static/media/img4.d01a9046.jpg"
+  },
+  {
+    "revision": "c50060a7a0f5e592ff8213f7803f4d49",
+    "url": "/Simba-Academy/static/media/img5.c50060a7.jpg"
+  },
+  {
+    "revision": "8a50dce28082a27e9f1aa8622296dfd9",
+    "url": "/Simba-Academy/static/media/img6.8a50dce2.jpg"
+  },
+  {
+    "revision": "09111942325390198204c61813772f23",
+    "url": "/Simba-Academy/static/media/img7.09111942.jpg"
+  },
+  {
+    "revision": "a4ee57993e8a75bd9921a5a5f658569a",
+    "url": "/Simba-Academy/static/media/img8.a4ee5799.jpg"
+  },
+  {
+    "revision": "517b080f8e024808ae4c26638dfd2894",
+    "url": "/Simba-Academy/static/media/img9.517b080f.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Simba-Academy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Simba-Academy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Simba-Academy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Simba-Academy/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "afe69dea92475437f6a36cd288767845",
+    "url": "/Simba-Academy/static/media/vacant.afe69dea.jpg"
+  }
+]);
